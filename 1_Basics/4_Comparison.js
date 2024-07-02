146e3c49b5bbec3c98a4b02cbbf5b2d7a311d929
Manjeet <157511865+ManjeetBaliyan@users.noreply.github.com>
2024-07-02 01:08:09 +0000

    Comparision

---
+++ 1_Basics/4_Comparison.js
@@ -0,0 +1,56 @@
+
+
+//Compresion always return boolean value
+
+
+// -----------------------------------------------Basics Compresion (Compare with same datatype)---------------------------------------------
+console.log(3>=10)
+console.log(3>10)
+console.log(3<=10)
+console.log(3<10)
+console.log(3!=10)
+console.log(3==10)
+
+console.log(" ")
+
+// ------------------------------------------------Compare with different Datatype-----------------------------------------------------------
+// Here automatic typeconversion occur
+console.log("2">= 1)
+console.log("2"> 1)
+console.log("2"< 1)
+console.log("2"<= 1)
+console.log("2"== 1)
+
+console.log(" ")
+
+// ---------------------------------------------Compare with null----------------------------------------------------------------------------
+
+/*when we compare with null comparision operator == and other work differently
+    * All comparision operator except(==) treat null == 0.
+    * Where (==) operator treat null= NaN.
+*/
+console.log(null>0)
+console.log(null>=0)
+console.log(null<=0)
+console.log(null<0)
+console.log(null==0)
+
+console.log(" ")
+
+//-------------------------------------------Compare with undefined--------------------------------------------------------------------------
+
+// Same condition as null but it's always return false. and all the operators wor similarlly
+console.log(undefined>0)
+console.log(undefined>=0)
+console.log(undefined<=0)
+console.log(undefined<0)
+console.log(undefined==0)
+
+console.log(" ")
+//--------------------------------------------(===)------------------------------------------------------------------------------------------
+ // It compare both (datatype and value), where (==) compare only value.
+
+ console.log("34"=== 34)
+
+
+ // NOTE => We always avoid to compare with null and undefined bcz it's reduce the readability of our code and it's not a good practices.
