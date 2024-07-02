cc7a9bb23a755fa42fcb0325df16d939fc47d04f
Manjeet <157511865+ManjeetBaliyan@users.noreply.github.com>
2024-07-02 01:10:50 +0000

    Datatype

--- 1_Basics/2_datatype.js
+++ 1_Basics/2_datatype.js
@@ -4,16 +4,16 @@
 (A)---> alert("hello") or alert (3+3)  Alert doesn't work here bcz we use node js not a browser. If we use a 
         browser then it will be functionable.
 
-(B)---> JS doesn't avoid space.
+(B)---> JS avoid space.
 
 (C)---> ----------------------DATATYPE IN JS---------------------
 
-        1.Number => 2 to the power 52(approx range).
+        1.Number => 2 to the power 52(approx range). Not seperate datatype for decimal all are considered as number.
         2.bigint => for very large value.
         3.boolean => true/false.
-        4.string => " " or ' '.
-        5.undefined =>
-        6.null => standalone value.
+        4.string => " Manjeet" or 'Manjeet '.
+        5.undefined => is a type of datatype in which we allocate the memory to the variable in the memory area but not given the value.
+        6.null => standalone value which not equal to 0.
         7.symbol => for uniqueness.     
 
 (D)---> typeof() is used to return datatype of variable or input.
