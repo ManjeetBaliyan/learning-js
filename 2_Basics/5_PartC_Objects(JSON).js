8aae35a1dec8a419cea29163bcd2a089cf2c3b32
Manjeet <157511865+ManjeetBaliyan@users.noreply.github.com>
2024-07-03 07:25:20 +0000

    PART (C) OBJECTS (JSON)

---
+++ 2_Basics/5_PartC_Objects(JSON).js
@@ -0,0 +1,19 @@
+/*------------------------------------- JSON (JAVASCRIPT OBJECT NOTATION)-----------------------------------------------------
+    ==> In jason all key are also trested as a string.
+    ==> JSON is also similier to object but it's doen't have  name.
+    ==> it can  be in the form of object and array.
+*/
+
+// Object form
+{
+    "name"; "manjeet",
+    "course":"JAVASCRIPT",
+    "fees": 2100
+}
+
+// Array form 
+[
+    {},// objects
+    {},
+    {}
+]
