e40deaf387b30d27b8a7a6fb11ddab61a2c420ba
Manjeet <157511865+ManjeetBaliyan@users.noreply.github.com>
2024-07-09 07:36:33 +0000

    SWITCH STATEMENT

---
+++ 4_Control_Flow/2_Switch_Statement.js
@@ -0,0 +1,45 @@
+const month = 10
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("Feb.");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+
+    // If break doesn't use then it will execute complete further code except thr default statement.
+     
+  default:
+    console.log("Please enter valid month")
+    break;
+}
