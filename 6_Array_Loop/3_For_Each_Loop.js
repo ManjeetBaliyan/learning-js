d3dbd7439818f12631e2348207231ab2619ba84d
Manjeet <157511865+ManjeetBaliyan@users.noreply.github.com>
2024-07-09 07:37:44 +0000

    FOR EACH LOOP

---
+++ 6_Array_Loop/3_For_Each_Loop.js
@@ -0,0 +1,52 @@
+//-------------------------   FOR EACH LOOP  ------------------------------------------------
+
+const lng=["js","java","cpp","python","ruby"]
+lng.forEach( function (element) {
+    console.log(`language is ${element}`)
+})
+
+
+// With Arrow function 
+
+lng.forEach((element) => {
+    console.log(element)
+});
+
+// By Decleration the function outside and pass the reference of that function inside the FOR EACH LOOP.
+
+function language(items)
+{
+    console.log(items)
+}
+
+lng.forEach(language) // Here only give reference( language ) not executed( language() ) here.
+
+
+// For each has more than one parameter 
+
+lng.forEach((element,index,lng) => {
+    console.log(element,index,lng)
+});
+
+
+// When a array contain a no. of objects 
+
+const arr=[
+    {
+        name: "java",
+        petname:"java"
+    },
+    {
+        name: "cpp",
+        petname:"c++"
+    },
+    {
+        name: "python",
+        petname:"py"
+    }
+]
+
+// Here element refer the objects of the array.
+arr.forEach((element) => {
+    console.log(`My Language is ${element.name} and pet name is ${element.petname}.` )
+});
