dae3c5c612938ca80fbcc0c37b5e65c0e3912bcb
Manjeet <157511865+ManjeetBaliyan@users.noreply.github.com>
2024-07-09 07:37:16 +0000

    WHILE AND DO-WHILE LOOP

---
+++ 5_Loops/2_While_Loop.js
@@ -0,0 +1,22 @@
+let i=0
+while (i<=10) {
+    console.log(`Value of i = ${i}`)
+    i =i+2
+}
+
+// Array
+let myArray=["Manjeet","Shiva","Virat"]
+let arr=0;
+while (arr<myArray.length) {
+   console.log(`Value of array index is ${myArray[arr]}`)
+   arr++
+}
+
+
+// Do While Loop
+
+let score= 11
+do {
+    console.log(`Score is ${score}`)
+    score++
+} while (score<=10);
